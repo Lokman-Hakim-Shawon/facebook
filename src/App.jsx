8fb303684e5a954f2592e4e9e1cdd29c8a3c481a
lokman hakim <lokmanhakimshawon3@gmail.com>
2025-04-28 23:11:14 +0600

    add api in home page

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@ function App() {
   return (
     <div className='flex flex-col'>
     <Navbar click={button}/>
-    <div className='pt-24 lg:pt-12 h-full w-full lg:flex'>
+    <div className='pt-24 lg:pt-12 h-full w-full lg:flex lg:justify-between'>
     <div className={`${click===true? 'hidden lg:block':''} fixed w-1/2 lg:w-2/6 top-11 lg:top-13 h-[calc(100vh)] bg-gradient-to-r from-primary to-secondary overflow-auto`}>
       <SideBar routingData={data} />
     </div>
-    <div className='w-4/6'>
+    <div className='lg:ml-[33%] lg:w-4/6 '>
     <Outlet/>
     </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import Navbar from './../components/Navbar';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        fetch('/public/homeApi.json')
+        .then(res=>res.json())
+        .then(json=>setData(json))
+        .catch(err=>console.log("error fetching",err))
+    },[])
     return (
-        <div className=''>
-            {/* <Navbar/> */}
-           
-           home page 
-        </div>
+        <div className='h-full lg:flex lg:justify-between border-2 text-white '>
+        <div className='lg:h-screen overflow-y-auto w-full'>
+     {data.map((post, index) => (
+         <div key={index} className=' lg:m-5   lg:mx-32 bg-gradient-to-r from-primary to-secondary mb-4'>
+             <div className='lg:p-4'>
+             <div className='flex items-center'>
+             <img src={post.profile_pic} alt={`${post.name}'s profile`} width="50" className='p-2 rounded-full'/>
+             <h3 className='font-bold'>{post.name}</h3>
+             </div>
+             <p>{post.title}</p>
+             </div>
+             <div>
+             <img src={post.post_image} alt="Post"  className='w-full '/>
+             </div>
+             <div className='flex justify-between p-5'>
+                 <h1 className="">like</h1>
+                 <h1 className="">comment</h1>
+                 <h1 className="">share</h1>
+             </div>
+         </div>
+     ))}
+ </div>
+ 
+ </div> 
     );
 };
 
